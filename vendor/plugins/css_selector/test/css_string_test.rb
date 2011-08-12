b9aade4f0271753e1ca259c923e2a562542b5884
Mark D. Blackwell <markdblackwell01@gmail.com>
2011-08-12 11:00:47 -0400

    Move rest of CssString test code.

--- vendor/plugins/css_selector/test/css_string_test.rb
+++ vendor/plugins/css_selector/test/css_string_test.rb
@@ -18,6 +18,43 @@ class CssStringTest < Test::Unit::TestCase
     padded.concat(bare).zip(methods).each{|s,m| two_ways s, m.to_sym}
   end
 
+##  test "+" do
+  def test_plus
+    compare '', big.map{|e| CssString.new('v').+(*e) } # Keep without sugar.
+    compare '', big.map{|e| CssString.new('v') +  e  }
+  end
+
+##  test "ends" do
+  def test_ends
+    %w[ first last ].each{|e| two_ways ' > ', e.to_sym, ":#{e}-child"}
+  end
+
+##  test "not" do
+  def test_not
+# An empty array shouldn't alter it.
+    assert_equal 'v', (CssString.new('v').not [])
+# Increasing strings...
+    a = %w[  v  v:not(a)  v:not(a):not(b)  v:not(a):not(b):not(c)  ]
+# With an increasing list...
+    b = %w[  a  b  c ]
+    ((1..3).map{|n| b.take n}.unshift ['']).zip(a).each do |z,s|
+# Should match...
+# As arguments.
+      assert_equal s, (CssString.new('v').not *z)
+# As a passed array.
+      assert_equal s, (CssString.new('v').not  z)
+    end
+  end
+
+##  test "attribute" do
+  def test_attribute
+    assert_equal 'v', (CssString.new('v').attribute)
+    a = %w[ a b c d ]
+    arrays = (1..4).map{|n| a.take n}
+    strings = %w@  v[a]  v[a=b]  v[a=b][c]  v[a=b][c=d]  @
+    strings.zip(arrays).each{|s,a| two_ways_attribute s, a}
+  end
+
 #-------------
   private
 
@@ -44,6 +81,11 @@ class CssStringTest < Test::Unit::TestCase
   def two_ways joiner, method, append=''
     compare joiner, big.map{|e| CssString.new('v').send method, *e}, append
     compare joiner, big.map{|e| CssString.new('v').send method,  e}, append
+  end
+
+  def two_ways_attribute s, a
+    assert_equal s, (CssString.new('v').attribute *a)
+    assert_equal s, (CssString.new('v').attribute  a)
   end
 
 end
