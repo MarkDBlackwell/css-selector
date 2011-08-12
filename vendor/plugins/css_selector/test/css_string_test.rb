82ab41ba8e8666d539668849e4eaf65c4912502a
Mark D. Blackwell <markdblackwell01@gmail.com>
2011-08-12 10:54:55 -0400

    Move some test code.

--- vendor/plugins/css_selector/test/css_string_test.rb
+++ vendor/plugins/css_selector/test/css_string_test.rb
@@ -1,13 +1,49 @@
 require File.dirname(__FILE__) + '/test_helper'
 
-# class CssStringTest < ActiveSupport::TestCase
+## class CssStringTest < ActiveSupport::TestCase
 
 class CssStringTest < Test::Unit::TestCase
 
   include CssStringNamespace
 
-  def test_css_string_defined_if_included
-    assert defined? CssString
+  def test_after_app_includes_CssStringNamespace
+    assert defined?(CssString), 'missing CssString'
+  end
+
+##  test "common" do
+  def test_common
+    padded = [ ?> , ?+       ]. map{ |e| " #{e.chr} "}
+    bare   = [ ?. , ?# , ?\s ]. map{ |e|     e.chr   }
+    methods = %w[ child  adjacent    css_class  css_id  descend ]
+    padded.concat(bare).zip(methods).each{|s,m| two_ways s, m.to_sym}
+  end
+
+#-------------
+  private
+
+  def args; (0...number_of_arguments).map{|i|(?a + i).to_s} end
+  def number_of_arguments; 4 end
+
+  def bigger; sliding_take 1, (args.unshift 'v') end
+  def big   ; sliding_take 0, args               end
+
+  def compare joiner, objects, append=''
+    bigger.map{|e|(e.join joiner).concat append}.zip(objects).each do |s,o|
+# The result...
+# Should be a CssString:
+      flunk "#{o} not CssString" unless o.kind_of? CssString
+# Should be convertable to a string equal to the comparer:
+      assert_equal s, o
+    end
+  end
+
+  def sliding_take first, a
+    (first...first + number_of_arguments).map{|n| a.take n}
+  end
+
+  def two_ways joiner, method, append=''
+    compare joiner, big.map{|e| CssString.new('v').send method, *e}, append
+    compare joiner, big.map{|e| CssString.new('v').send method,  e}, append
   end
 
 end
--- vendor/plugins/css_selector/test/unit/css_string_test.rb
+++ vendor/plugins/css_selector/test/unit/css_string_test.rb
@@ -17,13 +17,6 @@ end
 
 class CssStringTest < ActiveSupport::TestCase
 
-  test "common" do
-    padded = [ ?> , ?+       ]. map{ |e| " #{e.chr} "}
-    bare   = [ ?. , ?# , ?\s ]. map{ |e|     e.chr   }
-    methods = %w[ child  adjacent    css_class  css_id  descend ]
-    padded.concat(bare).zip(methods).each{|s,m| two_ways s, m.to_sym}
-  end
-
 #=begin
   test "+" do
     compare '', big.map{|e| CssString.new('v').+(*e) } # Keep without sugar.
@@ -61,31 +54,6 @@ class CssStringTest < ActiveSupport::TestCase
 
 #-------------
   private
-
-  def bigger; sliding_take 1, (args.unshift 'v') end
-  def big   ; sliding_take 0, args               end
-
-  def args; (0...number_of_arguments).map{|i|(?a + i).to_s} end
-  def number_of_arguments; 4 end
-
-  def compare joiner, objects, append=''
-    bigger.map{|e|(e.join joiner).concat append}.zip(objects).each do |s,o|
-# The result...
-# Should be a CssString:
-      flunk "#{o} not CssString" unless o.kind_of? CssString
-# Should be convertable to a string equal to the comparer:
-      assert_equal s, o
-    end
-  end
-
-  def sliding_take first, a
-    (first...first + number_of_arguments).map{|n| a.take n}
-  end
-
-  def two_ways joiner, method, append=''
-    compare joiner, big.map{|e| CssString.new('v').send method, *e}, append
-    compare joiner, big.map{|e| CssString.new('v').send method,  e}, append
-  end
 
   def two_ways_attribute s, a
     assert_equal s, (CssString.new('v').attribute *a)
