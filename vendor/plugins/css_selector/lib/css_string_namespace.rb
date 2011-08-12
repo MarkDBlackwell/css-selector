3d1d24dd54068dbb558a8e6530e1112fc52dcdf0
Mark D. Blackwell <markdblackwell01@gmail.com>
2011-08-12 11:19:31 -0400

    Start modularizing.

--- vendor/plugins/css_selector/lib/css_string_namespace.rb
+++ vendor/plugins/css_selector/lib/css_string_namespace.rb
@@ -4,49 +4,61 @@ module CssStringNamespace
 
   class CssString < String
 
-    def +    *a; return self if guard *a; CssString.new [self, a].to_s end
-    def adjacent  *a; CssString.new a.flatten.unshift(self).join ' + ' end
-    def child     *a; CssString.new a.flatten.unshift(self).join ' > ' end
-    def css_class *a; CssString.new a.flatten.unshift(self).join  '.'  end
-    def css_id    *a; CssString.new a.flatten.unshift(self).join  '#'  end
-    def descend   *a; CssString.new a.flatten.unshift(self).join  ' '  end
-    def first     *a; self.child(*a) + ':first-child'                  end
-    def last      *a; self.child(*a) +  ':last-child'                  end
+    module InstanceMethods
 
-    def not *a
-      return self if guard *a
-      s = ':not('
-      self + s + (a.flatten.join ')' + s) + ')'
-    end
+      def +    *a; return self if guard *a; CssString.new [self, a].to_s end
+      def adjacent  *a; CssString.new a.flatten.unshift(self).join ' + ' end
+      def child     *a; CssString.new a.flatten.unshift(self).join ' > ' end
+      def css_class *a; CssString.new a.flatten.unshift(self).join  '.'  end
+      def css_id    *a; CssString.new a.flatten.unshift(self).join  '#'  end
+      def descend   *a; CssString.new a.flatten.unshift(self).join  ' '  end
+      def first     *a; self.child(*a) + ':first-child'                  end
+      def last      *a; self.child(*a) +  ':last-child'                  end
 
-    def attribute *array
-      return self if guard *array
-      r = self.clone
-      a = array.flatten
-      n_full_pairs, n_unpaired = a.length.divmod 2
-      (1..n_full_pairs + n_unpaired).each do |i|
-        r << '[' + a.shift
-        r << '=' + a.shift unless i > n_full_pairs
-        r << ']'
+      def not *a
+        return self if guard *a
+        s = ':not('
+        self + s + (a.flatten.join ')' + s) + ')'
       end
-      CssString.new r
-    end
+
+      def attribute *array
+        return self if guard *array
+        r = self.clone
+        a = array.flatten
+        n_full_pairs, n_unpaired = a.length.divmod 2
+        (1..n_full_pairs + n_unpaired).each do |i|
+          r << '[' + a.shift
+          r << '=' + a.shift unless i > n_full_pairs
+          r << ']'
+        end
+        CssString.new r
+      end
 
 #-------------
-    private
+      private
 
-    def guard *a
-      return true if a.blank?
-      a = a.flatten
-      a.blank? || ['']==a
+      def guard *a
+        return true if a.blank?
+        a = a.flatten
+        a.blank? || ['']==a
+      end
+
+    end
+
+    include InstanceMethods
+
+  end
+
+  module Constants
+
+    %w[ A BODY DIV FORM HEAD HTML IMG INPUT LABEL SCRIPT STYLE TABLE TITLE
+        ].each do |e|
+      self.const_set e.upcase.to_sym, (CssString.new e.downcase)
     end
 
   end
 
-  %w[ A BODY DIV FORM HEAD HTML IMG INPUT LABEL SCRIPT STYLE TABLE TITLE
-      ].each do |e|
-    self.const_set e.upcase.to_sym, (CssString.new e.downcase)
-  end
+  include Constants
 
 end
 #@-leo
