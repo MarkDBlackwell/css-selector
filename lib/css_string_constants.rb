a6ab84ffb9994575de55cb713f3e96f9a59f0ab5
Mark D. Blackwell <markdblackwell01@gmail.com>
2011-08-24 11:13:17 -0400

    Fix syntax error (using '%w').

--- lib/css_string_constants.rb
+++ lib/css_string_constants.rb
@@ -8,9 +8,10 @@ module CssStringConstants
     A ACTION BODY CLASS COMMIT DIV
     FORM HEAD HREF HTML IMG INPUT LABEL NAME NONE
     SCRIPT SRC STYLE SUBMIT TABLE TARGET TEXT TITLE TYPE VALUE
+        ].each{|e| self.const_set e.upcase.to_sym, (CssString.new e.downcase)}
+
 #        ].each do |e|
 #      self.const_set e.upcase.to_sym, (CssString.new e.downcase)
 #    end
-        ].each{|e| self.const_set e.upcase.to_sym, (CssString.new e.downcase)}
  
 end
